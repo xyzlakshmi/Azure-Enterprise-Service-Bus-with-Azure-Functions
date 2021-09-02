ae186fb7199ee8b6ed65c8b74d4607773b64a461
xyzlakshmi <89964197+xyzlakshmi@users.noreply.github.com>
2021-09-02 12:26:37 +0530

    Update AzureStoragePowerShell.ps1

--- POC/ARMTemplates/AzureStoragePowerShell.ps1
+++ POC/ARMTemplates/AzureStoragePowerShell.ps1
@@ -1,10 +1,1 @@
-﻿Install-Module AzureRM -Scope CurrentUser 
- $UserName = 'Manjunath_s07@infosys.com'
- $pass = 'Infy$123'
- $subscriptionId = '02a0c632-86b4-4972-8b2a-c20752d297f8'
-
-Login-AzureRmAccount 
-Select-AzureRmSubscription -SubscriptionId $subscriptionId
-
-New-AzureRmResourceGroup -Name testgroup -Location "Central India"
-New-AzureRmResourceGroupDeployment -ResourceGroupName testgroup -TemplateFile D:\Architecture\azure\AzureResourceTemplates\AzureServiceBus_ARM.json  -TemplateParameterFile D:\Architecture\azure\AzureResourceTemplates\AzureServiceBus_ARM.Parameters.json
+﻿
